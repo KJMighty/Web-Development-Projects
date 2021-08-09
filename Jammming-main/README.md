647102c1009b4e78e046dcb3359b1ae499954b73
Kai J Mighty <67477643+KJMighty@users.noreply.github.com>
2021-08-09 14:04:53 +0100

    Add files via upload

---
+++ Jammming-main/README.md
@@ -0,0 +1,2 @@
+# Jammming
+React project that allows user to add songs to spotify API
---
+++ Jammming-main/src/components/Track/Track.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import './Track.css';
+
+class Track extends React.Component {
+constructor(props) {
+  super(props)
+
+  this.addTrack = this.addTrack.bind(this);
+  this.removeTrack = this.removeTrack.bind(this);
+}
+
+addTrack(event) {
+  this.props.onAdd(this.props.track);
+}
+
+removeTrack(event) {
+  this.props.onRemove(this.props.track);
+}
+
+
+    renderAction() {
+      if(this.props.isRemoval) {
+          return <button className='Track-Action' onClick={this.removeTrack}>-</button>
+      } else {
+          return <button className='Track-Action' onClick={this.addTrack}>+</button>
+      }
+    }
+  
+    render() {
+        return (
+            <div className="Track">
+            <div className="Track-information">
+              <h3>{this.props.track.name}</h3>
+              <p>{this.props.track.artist} | {this.props.track.album}</p>
+            </div>
+            {this.renderAction()}
+          </div>
+          
+        )
+    }
+}
+
+export default Track;
